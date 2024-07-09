0abff12eb0002eb91bab940b597caa3b535cfc49
naluisag <naluisagdocs@gmail.com>
2024-07-09 20:35:12 -0300

    Add files via upload

---
+++ QuadradoMagico3x3/QuadradoMagico3x3/frmQuadrado.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace QuadradoMagico3x3
+{
+    partial class frmQuadrado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.flpQuadrado = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExecutar.Location = new System.Drawing.Point(138, 392);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(136, 36);
+            this.btnExecutar.TabIndex = 0;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // flpQuadrado
+            // 
+            this.flpQuadrado.BackColor = System.Drawing.Color.Gainsboro;
+            this.flpQuadrado.Location = new System.Drawing.Point(53, 45);
+            this.flpQuadrado.Name = "flpQuadrado";
+            this.flpQuadrado.Size = new System.Drawing.Size(319, 320);
+            this.flpQuadrado.TabIndex = 1;
+            // 
+            // frmQuadrado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(430, 467);
+            this.Controls.Add(this.flpQuadrado);
+            this.Controls.Add(this.btnExecutar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmQuadrado";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quadrado Mágico 3x3";
+            this.Load += new System.EventHandler(this.frmQuadrado_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.FlowLayoutPanel flpQuadrado;
+    }
+}
+
---
+++ QuadradoMagico3x3/QuadradoMagico3x3/frmQuadrado.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuadradoMagico3x3
+{
+    public partial class frmQuadrado : Form
+    {
+        int[,] mat = new int[3, 3];
+        int l = 0;
+        int c = 1;
+        int nome = 1;
+
+        public frmQuadrado()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    flpQuadrado.Controls.Add(CriarBotao(i,j));
+                }
+            }
+            
+        }
+
+        private Button CriarBotao(int linha, int coluna)
+        {
+            Button btnElemento = new Button();
+            btnElemento.Name = "btn" + nome;
+            btnElemento.Font = new Font("Arial", 30);
+            btnElemento.Size = new Size(100, 100);
+            btnElemento.BackColor = Color.LightGray;
+            btnElemento.FlatStyle = FlatStyle.Flat;
+            btnElemento.FlatAppearance.MouseOverBackColor = Color.Red;
+            btnElemento.Text = mat[linha,coluna].ToString();
+            nome++;
+            return btnElemento;
+        }
+
+        private void frmQuadrado_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (i == 0)
+                {
+                    mat[l, c] = i + 1;
+                }
+                else if (l - 1 < 0 && c + 1 > 2)
+                {
+                    l++;
+                    mat[l, c] = i + 1;
+                }
+                else if (l - 1 < 0)
+                {
+                    l = 2;
+                    c++;
+                    mat[l, c] = i + 1;
+                }
+                else if (c + 1 > 2)
+                {
+                    l--;
+                    c = 0;
+                    mat[l, c] = i + 1;
+                }
+                else if (mat[l - 1, c + 1] != 0)
+                {
+                    l++;
+                    mat[l, c] = i + 1;
+                }
+                else
+                {
+                    l--;
+                    c++;
+                    mat[l, c] = i + 1;
+                }
+            }
+        }
+    }
+}
